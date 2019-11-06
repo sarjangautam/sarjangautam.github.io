c708b1b4baae47b3fea0d062c91bbd46526826a6
Sarjan Gautam <sarjan.gautam@kathmandu.co.nz>
2019-11-07 08:08:48 +1300

    New Website

--- precache-manifest.5f26b18cea8ef035e69144f52790735d.js
+++ precache-manifest.5f26b18cea8ef035e69144f52790735d.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "bcf931a8fefd5176503b26f987ad6a35",
-    "url": "/sarjangautam/Profile/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "23c26b55c319c89e505a",
-    "url": "/sarjangautam/Profile/static/css/main.584f321a.chunk.css"
+    "url": "/static/css/main.584f321a.chunk.css"
   },
   {
     "revision": "154ced39631a829a30f7",
-    "url": "/sarjangautam/Profile/static/js/2.628ab0fb.chunk.js"
+    "url": "/static/js/2.628ab0fb.chunk.js"
   },
   {
     "revision": "23c26b55c319c89e505a",
-    "url": "/sarjangautam/Profile/static/js/main.e1f56f5e.chunk.js"
+    "url": "/static/js/main.e1f56f5e.chunk.js"
   },
   {
     "revision": "74127c686cb21f7f67b0",
-    "url": "/sarjangautam/Profile/static/js/runtime-main.a7ed7b97.js"
+    "url": "/static/js/runtime-main.a7ed7b97.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sarjangautam/Profile/precache-manifest.5f26b18cea8ef035e69144f52790735d.js"
+  "/precache-manifest.5f26b18cea8ef035e69144f52790735d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sarjangautam/Profile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
